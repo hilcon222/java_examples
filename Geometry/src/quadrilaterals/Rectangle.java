d114fcf3cb792d4ada416b7851939277f099bad7
Etosino <elia.toselli3@gmail.com>
2023-11-02 18:15:22 +0100

    Created class Rectangle, with no methods. Only a stub, but with a working constructor.

---
+++ Geometry/src/quadrilaterals/Rectangle.java
@@ -0,0 +1,14 @@
+package quadrilaterals;
+import point.Point;
+import java.math.*;
+
+public class Rectangle {
+	public double width;
+	public double height;
+	public Point center;
+	public Rectangle(double w, double h, Point center) {
+		this.width = w;
+		this.height = h;
+		this.center = center;
+	}
+}
